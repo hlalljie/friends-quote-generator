3aa426485d7e9d0412de148aef76766405d7edda
hlalljie <hlalljie@gmail.com>
2023-11-07 23:14:08 -0800

    Initial styling done

--- src/App.js
+++ src/App.js
@@ -8,14 +8,18 @@ function App() {
   );
 }
 
-var quotes = [{quote: "How you doin?", author: "Joey Trevioni"}]
-var images = {"Joey Trevioni": "https://static.wikia.nocookie.net/friends/images/f/f5/JoeyTribbiani.jpg/revision/latest?cb=20180424154245"}
+var quotes = [{quote: "HOW YOU DOIN?", author: "Joey Trevioni"}]
+var images = {"Joey Trevioni": "https://static.wikia.nocookie.net/friends/images/f/f5/JoeyTribbiani.jpg"}
 function QuoteBox() {
   return (
     <div className="quote-box" id="quote-box">
-      <h3 className="quote">{quotes[0].quote}</h3>
-      <p className="author">{quotes[0].author}</p>
-      <img src="https://static.wikia.nocookie.net/friends/images/f/f5/JoeyTribbiani.jpg"/>
+      <div className="quote-text">
+        <h3 className="quote">{quotes[0].quote}</h3>
+        <p className="author">{quotes[0].author}</p>
+      </div>
+      <div className="portrait-container">
+        <img className="portrait-image" src={images[quotes[0].author]}/>
+      </div>
     </div>
   );
 }
