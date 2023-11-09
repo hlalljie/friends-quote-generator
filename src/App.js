5b63f35753ea8477dff5dc537895c8c7d1643c7a
hlalljie <hlalljie@gmail.com>
2023-11-09 15:43:05 -0800

    change images to a dict with arrs for each person

--- src/App.js
+++ src/App.js
@@ -22,12 +22,12 @@ var quotes = [
   {quote: "And I have to live with a boy!", author: "Monica Geller"}
 ];
 var images = {
-  "Joey Tribbiani": {image: "https://images.entertainment.ie/uploads/2021/05/27152450/Joey-Tribbiani-best-Friends-character.jpg?w=1280&h=768&q=high"},
-  "Chandler Bing": {image: "https://pyxis.nymag.com/v1/imgs/079/792/3ed0d94be0a9bd3d023f00532889bab152-30-chandler-bing.rsquare.w330.jpg"},
-  "Monica Geller": {image: "https://i.insider.com/5c8279ebeb3ce821ef1247a2?width=1500"},
-  "Rachel Green": {image: "https://i.pinimg.com/736x/9b/e8/dc/9be8dcb0e1a9a8f0c080d798be3cdb58.jpg"},
-  "Ross Geller": {image: "https://i.insider.com/5a7e1faeaee63c28008b468e?width=700"},
-  "Pheobe Buffay": {image: "https://staticg.sportskeeda.com/editor/2021/12/8def6-16405297121221-1920.jpg"}
+  "Joey Tribbiani": ["https://images.entertainment.ie/uploads/2021/05/27152450/Joey-Tribbiani-best-Friends-character.jpg?w=1280&h=768&q=high"],
+  "Chandler Bing": ["https://pyxis.nymag.com/v1/imgs/079/792/3ed0d94be0a9bd3d023f00532889bab152-30-chandler-bing.rsquare.w330.jpg"],
+  "Monica Geller": ["https://i.insider.com/5c8279ebeb3ce821ef1247a2?width=1500"],
+  "Rachel Green": ["https://i.pinimg.com/736x/9b/e8/dc/9be8dcb0e1a9a8f0c080d798be3cdb58.jpg"],
+  "Ross Geller": ["https://i.insider.com/5a7e1faeaee63c28008b468e?width=700"],
+  "Pheobe Buffay": ["https://staticg.sportskeeda.com/editor/2021/12/8def6-16405297121221-1920.jpg"]
 }
 function App() {
   return (
@@ -84,7 +84,7 @@ class QuoteBox extends React.Component {
 
     // Image props
     let currentColor = colors[this.state.colorIndex];
-    let currentImage = images[renderQuote.author].image;
+    let currentImage = images[renderQuote.author][0];
     let imgAlt = renderQuote.author + " from Friends TV show";
 
     return (
